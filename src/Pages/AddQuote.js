f7e9878d8507a54500ac69ce81927105ed679fac
m-waqas-ashfaq <86121522+m-waqas-ashfaq@users.noreply.github.com>
2022-04-19 01:27:24 +0500

    Update AddQuote.js

--- src/Pages/AddQuote.js
+++ src/Pages/AddQuote.js
@@ -5,12 +5,8 @@ export const AddQuote = () => {
             <div className="sticky-con normal my-5 pzip">
                 <div className="container-fluid quote-form border-0">
                     <div className="row justify-content-center">
-                        <div className="col-12 mb-3">
+                        <div className="col-12 mb-3 mt-5">
                             <h1 className="text-center">Easy to Einsure</h1>
-                        </div>
-                        {/* <!--end of col-12--> */}
-                        <div className="col-12 mb-5">
-                            {/* <!-- <h2 className="text-center">Affordable Insurance made easy</h2>  --> */}
                         </div>
                         {/* <!--end of col-12--> */}
                         <div className="col-12 mb-3">
